eb64e2b94be4bcdffc410d49846fa54b968b4c93
Wong Jiang Fung <kakarukeys@gmail.com>
2014-04-12 21:59:42 +0800

    improved check_for_missing_date check for parsing error

--- fa/sanity.py
+++ fa/sanity.py
@@ -1,8 +1,8 @@
 def check_for_missing_date(df, tolerance):
-    """ Returns a series (date -> diff of it with previous date) where the diff is greater than a tolerance
+    """ Returns a collection of dates where the diff with previous date is greater than a tolerance.
         df: DataFrame object which index contains the dates to be checked.
         tolerance: numpy.timedelta64 object.
     """
     dates = df.index.to_series()
     date_change = dates - dates.shift(1)
-    return date_change[date_change > tolerance]
+    return date_change[date_change > tolerance].index
--- scripts/check.py
+++ scripts/check.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pandas.parser import CParserError
 
 from fa.loader import load_historical_data, load_financial_data, FINANCIAL_REPORT_TYPES
 from fa.sanity import check_for_missing_date
@@ -9,24 +10,31 @@ from settings import archive_directory, symbols
 """ Perform sanity check on all the data in the archive """
 
 def print_result(result):
-    if result.empty:
-        print("No missing date.")
+    for date in result:
+        print("Missing date before {0}.".format(date))
     else:
-        for date in result.index:
-            print("Missing date before {0}.".format(date))
+        print("No missing date.")
 
 if __name__ == "__main__":
     for s in symbols:
         print("Symbol: " + s)
         print("historical: ", end='')
 
-        df = load_historical_data(archive_directory, s)
-        print_result(check_for_missing_date(df, np.timedelta64(34, 'D')))
+        try:
+            df = load_historical_data(archive_directory, s)
+        except CParserError as e:
+            print("Error during parsing. " + str(e))
+        else:
+            print_result(check_for_missing_date(df, np.timedelta64(34, 'D')))
 
         for report_type in FINANCIAL_REPORT_TYPES:
             print(report_type + ": ", end='')
 
-            df = load_financial_data(archive_directory, report_type, s)
-            print_result(check_for_missing_date(df, np.timedelta64(366, 'D')))
+            try:
+                df = load_financial_data(archive_directory, report_type, s)
+            except ValueError as e:
+                print("Error during parsing. " + str(e))
+            else:
+                print_result(check_for_missing_date(df, np.timedelta64(366, 'D')))
 
         print()
