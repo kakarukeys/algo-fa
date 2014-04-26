b984abccf9d9aa1832f845449349f7ee296fed66
Wong Jiang Fung <kakarukeys@gmail.com>
2014-04-26 16:13:49 +0800

    added 4th graph - all the profit margins improved metric module added DatedMetric class added capability to calculate other profit margins than gross

--- fa/metric.py
+++ fa/metric.py
@@ -3,100 +3,162 @@ from .calculator import translate_index
 from .loader import load_historical_data, load_financial_data_all
 
 
+PROFIT_MARGIN_KINDS = ("gross", "operating", "pretax", "net")
+
 class Metric(object):
-	def __init__(self,
-			historical,
-			balance_sheet,
-			income_statement,
-			cash_flow,
-			financial_report_preparation_lag=np.timedelta64(30, 'D')
-		):
-		""" Returns an object that is capable of calculating various metrics of the stock.
-			historical, balance_sheet, income_statement, cash_flow: respective DataFrame objects
-			financial_report_preparation_lag (np.timedelta64 object):
-				how long does it take to release the financial report after the end of fiscal year?
-				default: 30 days.
-		"""
-		self.historical = historical
-		self.balance_sheet = balance_sheet
-		self.income_statement = income_statement
-		self.cash_flow = cash_flow
-		self.financial_report_preparation_lag = financial_report_preparation_lag
+    def __init__(self,
+            historical=None,
+            balance_sheet=None,
+            income_statement=None,
+            cash_flow=None,
+            financial_report_preparation_lag=np.timedelta64(30, 'D')
+        ):
+        """ Returns an object that is capable of calculating various metrics of the stock.
+            historical, balance_sheet, income_statement, cash_flow: respective DataFrame objects, default: None
+            financial_report_preparation_lag (np.timedelta64 object):
+                how long does it take to release the financial report after the end of fiscal year?
+                default: 30 days.
+        """
+        self.historical = historical
+        self.balance_sheet = balance_sheet
+        self.income_statement = income_statement
+        self.cash_flow = cash_flow
+        self.financial_report_preparation_lag = financial_report_preparation_lag
 
-		# historical data with missing dates filled in, with values from future
-		self.historical_fillforward = historical.resample('1D', fill_method="ffill")
+        if historical is not None:
+            # historical data with missing dates filled in, with values from future
+            self.historical_fillforward = historical.resample('1D', fill_method="ffill")
 
-		# historical data with missing dates filled in, with values from past
-		self.historical_fillbackward = historical.resample('1D', fill_method="bfill")
+            # historical data with missing dates filled in, with values from past
+            self.historical_fillbackward = historical.resample('1D', fill_method="bfill")
 
-		# financial reports with the lag in information release factored in
-		self.balance_sheet_lagged = translate_index(balance_sheet, self.financial_report_preparation_lag)
-		self.income_statement_lagged = translate_index(income_statement, self.financial_report_preparation_lag)
-		self.cash_flow_lagged = translate_index(cash_flow, self.financial_report_preparation_lag)
+        # financial reports with the lag in information release factored in
+        if balance_sheet is not None:
+            self.balance_sheet_lagged = translate_index(balance_sheet, self.financial_report_preparation_lag)
 
-	@classmethod
-	def from_archive(cls, archive_directory, symbol, **kwargs):
-		""" Returns an object that is capable of calculating various metrics of the stock.
-			archive_directory: will load data from this directory to construct the object
-			symbol: e.g. "C6L"
-			Any extra keyword arguments will be passed to the constructor.
-		"""
-		historical_data = load_historical_data(archive_directory, symbol)
-		financial_data = load_financial_data_all(archive_directory, symbol)
+        if income_statement is not None:
+            self.income_statement_lagged = translate_index(income_statement, self.financial_report_preparation_lag)
 
-		return cls(
-			historical_data,
-			financial_data["balance-sheet"],
-			financial_data["income-statement"],
-			financial_data["cash-flow"],
-			**kwargs
-		)
+        if cash_flow is not None:
+            self.cash_flow_lagged = translate_index(cash_flow, self.financial_report_preparation_lag)
 
-	def calc_return(self, buy_date, sell_date, price_column="Adj Close"):
-		""" Returns the return of the stock if a buy market order is made on <buy_date> and a sell market order is
-			made on <sell_date>.
-			buy_date, sell_date: pd.Timestamp object
-			price_column: the column to get prices, default: "Adj Close".
-		"""
-		# use fillbackward here because a market order is executed as soon as a price is available
-		buy_price = self.historical_fillbackward.at[buy_date, price_column]
-		sell_price = self.historical_fillbackward.at[sell_date, price_column]
-		return sell_price / buy_price - 1
+    @classmethod
+    def from_archive(cls, archive_directory, symbol, *args, **kwargs):
+        """ Returns an object that is capable of calculating various metrics of the stock.
+            archive_directory: will load data from this directory to construct the object
+            symbol: e.g. "C6L"
+            Any extra arguments will be passed on to the constructor.
+        """
+        historical_data = load_historical_data(archive_directory, symbol)
+        financial_data = load_financial_data_all(archive_directory, symbol)
+        kwargs.update({
+            "historical": historical_data,
+            "balance_sheet": financial_data["balance-sheet"],
+            "income_statement": financial_data["income-statement"],
+            "cash_flow": financial_data["cash-flow"],
+        })
 
-	def calc_annual_return(self, buy_date, sell_date, price_column="Adj Close"):
-		""" Returns the annualized return of the stock if a buy market order is made on <buy_date> and a sell market order is
-			made on <sell_date>.
-			buy_date, sell_date: pd.Timestamp object
-			price_column: the column to get prices, default: "Adj Close".
-		"""
-		ret = self.calc_return(buy_date, sell_date, price_column)
-		conversion_factor = np.timedelta64(365, 'D') / (np.datetime64(sell_date) - np.datetime64(buy_date))
-		return ret * conversion_factor
+        return cls(*args, **kwargs)
 
-	def calc_pe_ratio(self, date=None, price_column="Adj Close", eps_column="EPS (Basic)"):
-		""" Returns the P/E Ratio of the stock.
-			date: if passed, will return scalar value at it, else return series, default: None.
-			price_column: the column to get prices, default: "Adj Close".
-			eps_columm: the column to get EPS, default: "EPS (Basic)"
-		"""
-		if date is None:
-			eps = self.income_statement_lagged[eps_column]
-			pps = self.historical_fillforward.loc[eps.index, price_column]	# use fillforward here to avoid peeking into future
-		else:
-			eps = self.income_statement_lagged.at[date, eps_column]
-			pps = self.historical_fillforward.at[date, price_column]
+    def at(self, date):
+        """ Fixes the internal date to <date> to be used for calculation of metrics. """
+        dm = DatedMetric(date)
+        dm.__dict__.update(self.__dict__)   # bring over all attributes
+        return dm
 
-		return pps / eps
+    def _calc_return(self, buy_date, sell_date, price_column):
+        # use fillbackward here because a market order is executed as soon as a price is available
+        buy_price = self.historical_fillbackward.at[buy_date, price_column]
+        sell_price = self.historical_fillbackward.at[sell_date, price_column]
+        return sell_price / buy_price - 1
 
-	def calc_gross_profit_margin(self, date=None):
-		""" Returns the Gross Profit Margin of the stock.
-			date: if passed, will return scalar value at it, else return series, default: None.
-		"""
-		if date is None:
-			gross_profit = self.income_statement_lagged["Gross Income"]
-			revenue = self.income_statement_lagged["Sales/Revenue"]
-		else:
-			gross_profit = self.income_statement_lagged.at[date, "Gross Income"]
-			revenue = self.income_statement_lagged.at[date, "Sales/Revenue"]
+    def calc_return(self, buy_date, sell_date, price_column="Adj Close"):
+        """ Returns the return of the stock if a buy market order is made on <buy_date> and a sell market order is
+            made on <sell_date>.
+            buy_date, sell_date: pd.Timestamp object
+            price_column: the column to get prices, default: "Adj Close".
+        """
+        return self._calc_return(buy_date, sell_date, price_column)
 
-		return gross_profit / revenue
+    def calc_annual_return(self, buy_date, sell_date, price_column="Adj Close"):
+        """ Returns the annualized return of the stock if a buy market order is made on <buy_date> and a sell market order is
+            made on <sell_date>.
+            buy_date, sell_date: pd.Timestamp object
+            price_column: the column to get prices, default: "Adj Close".
+        """
+        ret = self._calc_return(buy_date, sell_date, price_column)
+        conversion_factor = np.timedelta64(365, 'D') / (np.datetime64(sell_date) - np.datetime64(buy_date))
+        return ret * conversion_factor
+
+    def calc_pe_ratio(self, price_column="Adj Close", eps_column="EPS (Basic)"):
+        """ Returns the P/E Ratio series of the stock.
+            price_column: the column to get prices, default: "Adj Close".
+            eps_columm: the column to get EPS, default: "EPS (Basic)"
+        """
+        eps = self.income_statement_lagged[eps_column]
+        pps = self.historical_fillforward.loc[eps.index, price_column]  # use fillforward here to avoid peeking into future
+        return pps / eps
+
+    def calc_profit_margin(self, kind):
+        """ Returns the Profit Margin series of the stock.
+            kind: a string in PROFIT_MARGIN_KINDS
+        """
+        if kind == "operating":
+            profit = \
+                self.income_statement_lagged["Gross Income"] - \
+                self.income_statement_lagged["SG&A Expense"] - \
+                self.income_statement_lagged["Other Operating Expense"]
+        else:
+            profit = self.income_statement_lagged[kind.title() + " Income"]
+
+        revenue = self.income_statement_lagged["Sales/Revenue"]
+
+        return profit / revenue
+
+class DatedMetric(Metric):
+    def __init__(self, date, **kwargs):
+        """ Returns an object that is capable of calculating various metrics of the stock at <date>.
+            Takes arguments of Metric constructor as extra keyword arguments.
+        """
+        super(DatedMetric, self).__init__(**kwargs)
+        self.date = date
+
+    def calc_return(self, *args, **kwargs):
+        """ Returns the return of the stock if a buy market order is made on the internal date and a sell market order is
+            made on <sell_date>.
+            sell_date: pd.Timestamp object
+            price_column: the column to get prices, default: "Adj Close".
+        """
+        return super(DatedMetric, self).calc_return(self.date, *args, **kwargs)
+
+    def calc_annual_return(self, *args, **kwargs):
+        """ Returns the annualized return of the stock if a buy market order is made on the internal date and a sell market order is made on <sell_date>.
+            sell_date: pd.Timestamp object
+            price_column: the column to get prices, default: "Adj Close".
+        """
+        return super(DatedMetric, self).calc_annual_return(self.date, *args, **kwargs)
+
+    def calc_pe_ratio(self, price_column="Adj Close", eps_column="EPS (Basic)"):
+        """ Returns the P/E Ratio of the stock at the internal date.
+            price_column: the column to get prices, default: "Adj Close".
+            eps_columm: the column to get EPS, default: "EPS (Basic)"
+        """
+        eps = self.income_statement_lagged.at[self.date, eps_column]
+        pps = self.historical_fillforward.at[self.date, price_column]
+        return pps / eps
+
+    def calc_profit_margin(self, kind):
+        """ Returns the Profit Margin of the stock at the internal date.
+            kind: a string in PROFIT_MARGIN_KINDS
+        """
+        if kind == "operating":
+            profit = \
+                self.income_statement_lagged.at[self.date, "Gross Income"] - \
+                self.income_statement_lagged.at[self.date, "SG&A Expense"] - \
+                self.income_statement_lagged.at[self.date, "Other Operating Expense"]
+        else:
+            profit = self.income_statement_lagged.at[self.date, kind.title() + " Income"]
+
+        revenue = self.income_statement_lagged.at[self.date, "Sales/Revenue"]
+
+        return profit / revenue
--- scripts/explore.py
+++ scripts/explore.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from fa.metric import Metric
+from fa.metric import Metric, PROFIT_MARGIN_KINDS
 from fa.inspect import get_first_commonly_available_year
 from fa.plot_util import print_point_labels
 
@@ -26,24 +26,32 @@ with pd.plot_params.use('x_compat', True):
 		graph.set_xlabel("Date")
 		graph.set_ylabel("P/E Ratio")
 
-# prepare figure data
+# prepare graph data
 buy_year = get_first_commonly_available_year(pe_ratios_list)
-
 pe_ratios_at_buy_year_list = [ts.loc[str(buy_year)] for ts in pe_ratios_list]
 buy_dates = [ser.index[0] for ser in pe_ratios_at_buy_year_list]
 pe_ratios = [ser.iat[0] for ser in pe_ratios_at_buy_year_list]
 
-sell_date = pd.Timestamp(end_date) - np.timedelta64(1, 'D')
-percentage_annual_returns = [m.calc_annual_return(buy_date, sell_date) * 100 for m, buy_date in zip(metrics, buy_dates)]
-gross_profit_margins = [m.calc_gross_profit_margin(buy_date) * 100 for m, buy_date in zip(metrics, buy_dates)]
+dated_metrics = [m.at(buy_date) for m, buy_date in zip(metrics, buy_dates)]
 
-df = pd.DataFrame.from_dict({
+sell_date = pd.Timestamp(end_date) - np.timedelta64(1, 'D')
+percentage_annual_returns = [dm.calc_annual_return(sell_date) * 100 for dm in dated_metrics]
+
+profit_margin_labels = [k.title() + " Profit Margin (%)" for k in PROFIT_MARGIN_KINDS]
+profit_margins = {
+	label: [dm.calc_profit_margin(kind) * 100 for dm in dated_metrics]
+	for label, kind in zip(profit_margin_labels, PROFIT_MARGIN_KINDS)
+}
+
+graph_data = {
 	"Symbol": list(symbols.keys()),
 	"Name": list(symbols.values()),
 	"P/E Ratio": pe_ratios,
 	"Annual Return (%)": percentage_annual_returns,
-	"Gross Profit Margin (%)": gross_profit_margins,
-})
+}
+graph_data.update(profit_margins)
+
+df = pd.DataFrame.from_dict(graph_data)
 
 # figure 2
 title = "Annual Return of stocks bought in {0} sold at {1} over P/E Ratio\n".format(buy_year, sell_date.date())
@@ -57,6 +65,11 @@ graph.hlines(0, *xbound, color="red")
 title = "Gross Profit Margin of stocks in {0} over P/E Ratio\n".format(buy_year)
 graph = df.plot(x="P/E Ratio", y="Gross Profit Margin (%)", kind="scatter", title=title, figsize=(12, 10))
 print_point_labels(df, graph, x="P/E Ratio", y="Gross Profit Margin (%)", label="Name")
+
+# figure 4
+new_df = df[profit_margin_labels + ["Name"]].set_index("Name").transpose()
+title = "Profit Margins of stocks in {0}".format(buy_year)
+new_df.plot(subplots=True, kind="bar", rot=20, title=title, figsize=(8, 13))
 
 # show all figures
 plt.show()
