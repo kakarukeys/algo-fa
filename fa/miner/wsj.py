df2edc38c865854e56a975256c20a7db1f10b5ff
Wong Jiang Fung <kakarukeys@gmail.com>
2014-05-01 12:12:12 +0800

    tested and refactored wsj module

--- fa/miner/wsj.py
+++ fa/miner/wsj.py
@@ -5,7 +5,17 @@ from bs4 import BeautifulSoup
 WSJ_URL_TEMPLATE = "http://quotes.wsj.com/SG/{symbol}/financials/{timeframe}/{report_type}"
 FINANCIAL_REPORT_TYPES = ("balance-sheet", "cash-flow", "income-statement")
 
-def scrape_row(tr):
+def get_report_html(symbol, timeframe, report_type):
+	""" Returns report page HTML.
+		symbol: e.g. "C6L"
+		timeframe: "annual" or "quarter"
+		report_type: any string in FINANCIAL_REPORT_TYPES
+	"""
+	url = WSJ_URL_TEMPLATE.format(symbol=symbol, timeframe=timeframe, report_type=report_type)
+	r = requests.get(url)
+	return r.text
+
+def _scrape_row(tr):
 	return tr.select("td.rowTitle")[0].text.strip(), [e.text.strip() for e in tr.select("td.valueCell")]
 
 def get_financial_data(symbol, timeframe, report_type):
@@ -14,9 +24,8 @@ def get_financial_data(symbol, timeframe, report_type):
 		timeframe: "annual" or "quarter"
 		report_type: any string in FINANCIAL_REPORT_TYPES
 	"""
-	url = WSJ_URL_TEMPLATE.format(symbol=symbol, timeframe=timeframe, report_type=report_type)
-	r = requests.get(url)
-	soup = BeautifulSoup(r.text)
+	html = get_report_html(symbol, timeframe, report_type)
+	soup = BeautifulSoup(html)
 	tables = soup.select("table.crDataTable")
 	table_header = tables[0].select("tr.topRow > th")
 
@@ -30,6 +39,6 @@ def get_financial_data(symbol, timeframe, report_type):
 		]
 	}
 
-	result["data"] += [scrape_row(tr) for tbl in tables for tr in tbl.tbody.select("tr")]
+	result["data"] += [_scrape_row(tr) for tbl in tables for tr in tbl.tbody.select("tr")]
 
 	return result
---
+++ tests/miner/test_wsj.py
@@ -0,0 +1,42 @@
+import unittest
+from unittest.mock import patch, MagicMock
+
+import os.path
+from itertools import count
+
+from bs4 import BeautifulSoup
+
+from fa.miner import wsj
+
+
+class TestWSJ(unittest.TestCase):
+    def test_get_report_html(self):
+        class MockResponse(object):
+            """ HTTP response from opening report page """
+            text = "report text"
+
+        with patch("fa.miner.wsj.requests.get", MagicMock(return_value=MockResponse())) as mock_get:
+            html = wsj.get_report_html("C6L", "annual", "balance-sheet")
+
+        mock_get.assert_called_once_with("http://quotes.wsj.com/SG/C6L/financials/annual/balance-sheet")
+        self.assertEqual(html, "report text")
+
+    def test_get_financial_data(self):
+        fixture_data_filename = os.path.join(os.path.dirname(__file__), "fixture_data", "C6L_annual_balance-sheet.html")
+        with open(fixture_data_filename) as f:
+            html = f.read()
+
+        with patch("fa.miner.wsj.get_report_html", MagicMock(return_value=html)) as mock_get_report_html:
+            result = wsj.get_financial_data("C6L", "annual", "balance-sheet")
+
+        mock_get_report_html.assert_called_once_with("C6L", "annual", "balance-sheet")
+        self.assertEqual(result["symbol"], "C6L")
+        self.assertEqual(result["timeframe"], "annual")
+        self.assertEqual(result["report_type"], "balance-sheet")
+        self.assertEqual(result["top_remark"], "Fiscal year is April-March. All values SGD Millions.")
+        self.assertEqual(result["data"][0], ("periods", ["2009", "2010", "2011", "2012", "2013"]))
+        self.assertEqual(result["data"][1], ("Cash & Short Term Investments", ["4,504", "4,612", "7,832", "5,400", "5,488"]))
+        self.assertEqual(len(result["data"]), 98)
+
+if __name__ == "__main__":
+    unittest.main()
