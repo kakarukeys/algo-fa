f1204209604ff41313938611b4e8f642f1fe58a5
Wong Jiang Fung <kakarukeys@gmail.com>
2014-05-05 22:31:15 +0800

    completed testing for finance module

--- tests/analysis/test_finance.py
+++ tests/analysis/test_finance.py
@@ -85,5 +85,93 @@ class TestMetric(PandasTestCase):
         self.assertFrameEqual(pe_ratios1, pd.Series([2.5, 1.4, 1], index=expected_index))
         self.assertFrameEqual(pe_ratios2, pd.Series([0.5, 1.25, 0.625], index=expected_index))
 
+    def test_calc_profit_margin(self):
+        income_statement_data = {
+            "Gross Income": [62, 50, 36],
+            "Sales/Revenue": [3.1, 2.0, 1.2],
+            "SG&A Expense": [10, 20, 12],
+            "Other Operating Expense": [21, 2, 3],
+        }
+        income_statement_index = pd.to_datetime(["2012-01-01", "2012-01-02", "2012-01-03"])
+
+        metric = Metric(
+            income_statement=pd.DataFrame(income_statement_data, index=income_statement_index),
+            financial_report_preparation_lag=np.timedelta64(1, 'D')
+        )
+
+        gross_profit_margin = metric.calc_profit_margin("gross")
+        operating_profit_margin = metric.calc_profit_margin("operating")
+
+        # because of 1 day financial_report_preparation_lag
+        expected_index = pd.to_datetime(["2012-01-02", "2012-01-03", "2012-01-04"])
+        self.assertFrameEqual(gross_profit_margin, pd.Series([20, 25, 30], index=expected_index))
+        self.assertFrameEqual(operating_profit_margin, pd.Series([10, 14, 17.5], index=expected_index))
+
+class TestDatedMetric(unittest.TestCase):
+    def test_calc_return(self):
+        date = pd.Timestamp("2012-01-02")
+        dated_metric = DatedMetric(date)
+
+        # verify that the method is just partial application of the superclass' method
+        with patch("fa.analysis.finance.Metric.calc_return", MagicMock(return_value=0)) as mock_calc_return:
+            ret = dated_metric.calc_return(1, b=2)
+            mock_calc_return.assert_called_once_with(date, 1, b=2)
+
+        self.assertEqual(ret, 0)
+
+    def test_calc_annual_return(self):
+        date = pd.Timestamp("2012-01-02")
+        dated_metric = DatedMetric(date)
+
+        # verify that the method is just partial application of the superclass' method
+        with patch("fa.analysis.finance.Metric.calc_annual_return", MagicMock(return_value=0)) as mock_calc_annual_return:
+            ret = dated_metric.calc_annual_return(1, b=2)
+            mock_calc_annual_return.assert_called_once_with(date, 1, b=2)
+
+        self.assertEqual(ret, 0)
+
+    def test_calc_pe_ratio(self):
+        historical_data = {"Adj Close": [3.0, 3.5, 4.2], "Close": [3.0, 5.0, 4.2]}
+        historical_index = pd.to_datetime(["2012-01-01", "2012-01-03", "2012-01-05"])
+
+        income_statement_data = {"EPS (Basic)": [1.2, 2.5, 3.5], "EPS (Diluted)": [6.0, 4.0, 8.0]}
+        income_statement_index = pd.to_datetime(["2012-01-01", "2012-01-02", "2012-01-03"])
+
+        dated_metric = DatedMetric(
+            pd.Timestamp("2012-01-03"),
+            historical=pd.DataFrame(historical_data, index=historical_index),
+            income_statement=pd.DataFrame(income_statement_data, index=income_statement_index),
+            financial_report_preparation_lag=np.timedelta64(1, 'D')
+        )
+
+        pe_ratios1 = dated_metric.calc_pe_ratio()
+        pe_ratios2 = dated_metric.calc_pe_ratio("Close", "EPS (Diluted)")
+
+        # because of 1 day financial_report_preparation_lag
+        self.assertEqual(pe_ratios1, 1.4)
+        self.assertEqual(pe_ratios2, 1.25)
+
+    def test_calc_profit_margin(self):
+        income_statement_data = {
+            "Gross Income": [62, 50, 36],
+            "Sales/Revenue": [3.1, 2.0, 1.2],
+            "SG&A Expense": [10, 20, 12],
+            "Other Operating Expense": [21, 2, 3],
+        }
+        income_statement_index = pd.to_datetime(["2012-01-01", "2012-01-02", "2012-01-03"])
+
+        dated_metric = DatedMetric(
+            pd.Timestamp("2012-01-03"),
+            income_statement=pd.DataFrame(income_statement_data, index=income_statement_index),
+            financial_report_preparation_lag=np.timedelta64(1, 'D')
+        )
+
+        gross_profit_margin = dated_metric.calc_profit_margin("gross")
+        operating_profit_margin = dated_metric.calc_profit_margin("operating")
+
+        # because of 1 day financial_report_preparation_lag
+        self.assertEqual(gross_profit_margin, 25)
+        self.assertEqual(operating_profit_margin, 14)
+
 if __name__ == "__main__":
     unittest.main()
