b5c6b62850f3fbacf063cfbc2a06e4159f890c87
Wong Jiang Fung <kakarukeys@gmail.com>
2014-05-10 20:24:53 +0800

    improved assertFrameEqual to allow NaN comparison reuses Pandas util functions for assertion

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,41 +1,39 @@
 import unittest
+from unittest.mock import patch, MagicMock
+
 import pandas as pd
+import numpy as np
+
 from tests import util
 
 
 class TestPandasTestCase(unittest.TestCase):
     def test_assertFrameEqual__Series(self):
         ptc = util.PandasTestCase()
+        s1 = pd.Series()
+        s2 = pd.Series()
 
-        ptc.assertFrameEqual(pd.Series([1,2,3]), pd.Series([1,2,3]))
-        self.assertRaises(AssertionError, ptc.assertFrameEqual, pd.Series([1,2,4]), pd.Series([1,2,3]))
+        with patch("tests.util.assert_series_equal") as mock_assert_series_equal:
+            ptc.assertFrameEqual(s1, s2, b=1)
+            mock_assert_series_equal.assert_called_once_with(s1, s2, b=1)
+
+        with patch("tests.util.assert_series_equal", MagicMock(side_effect=AssertionError)) as mock_assert_series_equal:
+            self.assertRaises(AssertionError, ptc.assertFrameEqual, s1, s2)
 
     def test_assertFrameEqual__DataFrame(self):
         ptc = util.PandasTestCase()
+        df1 = pd.DataFrame()
+        df2 = pd.DataFrame()
 
-        ptc.assertFrameEqual(pd.DataFrame({'a': [1,2,3], 'b': [4,5,6]}), pd.DataFrame({'a': [1,2,3], 'b': [4,5,6]}))
-        self.assertRaises(
-            AssertionError,
-            ptc.assertFrameEqual,
-            pd.DataFrame({'a': [1,2,4], 'b': [4,5,6]}),
-            pd.DataFrame({'a': [1,2,3], 'b': [4,5,6]})
-        )
-
-        # Can only compare identically-labeled DataFrame objects
-        self.assertRaises(
-            AssertionError,
-            ptc.assertFrameEqual,
-            pd.DataFrame({'a': [1,2,3], 'b': [4,5,6]}),
-            pd.DataFrame({'a': [1,2,3], 'b': [4,5,6]}, index=[6,7,8])
-        )
+        with patch("tests.util.assert_frame_equal") as mock_assert_frame_equal:
+            ptc.assertFrameEqual(df1, df2, b=1)
+            mock_assert_frame_equal.assert_called_once_with(df1, df2, b=1)
 
     def test_assertFrameEqual__Panel(self):
         ptc = util.PandasTestCase()
-        df1 = pd.DataFrame({'a': [1,2,3], 'b': [4,5,6]})
-        df2 = pd.DataFrame({'a': [1,2,4], 'b': [4,5,6]})
-        p1 = pd.Panel({'A': df1, 'B': df1})
-        p2 = pd.Panel({'A': df1, 'B': df1})
-        p3 = pd.Panel({'A': df2, 'B': df1})
+        p1 = pd.Panel()
+        p2 = pd.Panel()
 
-        self.assertRaises(NotImplementedError, ptc.assertFrameEqual, p1, p2)
-        self.assertRaises(NotImplementedError, ptc.assertFrameEqual, p3, p2)
+        with patch("tests.util.assert_panel_equal") as mock_assert_panel_equal:
+            ptc.assertFrameEqual(p1, p2, b=1)
+            mock_assert_panel_equal.assert_called_once_with(p1, p2, b=1)
--- tests/util.py
+++ tests/util.py
@@ -1,27 +1,28 @@
 import unittest
-from pandas.core.generic import NDFrame
-from pandas import Panel
+
+from pandas import Series, DataFrame
+from pandas.util.testing import assert_series_equal, assert_frame_equal, assert_panel_equal
 
 
 class PandasTestCase(unittest.TestCase):
-    """ For test case with assertion involving Pandas frame objects """
-    def assertFrameEqual(self, first, second, msg=None):
-        """ Fail if the two frames are unequal at all values as determined by the '==' operator. """
-        if isinstance(first, Panel):
-            raise NotImplementedError("assertFrameEqual can't compare Panel objects yet!")
+    """ For test case with assertion involving Pandas NDFrame objects """
+    def assertFrameEqual(self, first, second, msg=None, **kwargs):
+        """ Fail if the two NDFrame objects of the same type are unequal at any value
+            as determined by the '==' operator (two NaNs are treated as equal).
 
-        e = self.failureException(msg or "\n{0}\n!=\n{1}".format(first, second))
+            It reuses assertion functions in pandas.util.testing internally, but produces
+            clearer failure message.
+
+            Any extra extra keyword arguments are passed on to the assertion function.
+        """
+        if isinstance(first, Series):
+            assert_func = assert_series_equal
+        elif isinstance(first, DataFrame):
+            assert_func = assert_frame_equal
+        else:
+            assert_func = assert_panel_equal
 
         try:
-            is_equal = first == second
-        except ValueError as ve:
-            if str(ve) == "Can only compare identically-labeled DataFrame objects":
-                raise e
-            else:
-                raise
-
-        while isinstance(is_equal, NDFrame):
-            is_equal = is_equal.all()
-
-        if not is_equal:
-            raise e
+            assert_func(first, second, **kwargs)
+        except AssertionError:
+            raise self.failureException(msg or "\n{0}\n!=\n{1}".format(first, second))
