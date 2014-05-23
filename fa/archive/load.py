9e3f8a2be19a388c41e65b5f8cecd114329f8cd3
Wong Jiang Fung <kakarukeys@gmail.com>
2014-05-23 23:14:11 +0800

    refactoring

--- fa/archive/load.py
+++ fa/archive/load.py
@@ -1,14 +1,11 @@
-import json, os, re
-from datetime import datetime, date
-from dateutil.relativedelta import relativedelta
-from functools import partial
-from collections import defaultdict
+import json, os
 
 import numpy as np
 import pandas as pd
 from pandas.io.parsers import read_csv
 
 from fa.miner.wsj import FINANCIAL_REPORT_TYPES
+from fa.archive.preprocess import preprocess
 
 
 HISTORICAL_DATA_COLUMNS = {
@@ -18,28 +15,6 @@ HISTORICAL_DATA_COLUMNS = {
 	"Close": np.float64,
 	"Volume": np.int64,
 	"Adj Close": np.float64,
-}
-
-TOP_REMARK_RE = re.compile(
-	r"Fiscal.+ [A-Za-z]+-(?P<fiscal_year_end_month>[A-Za-z]+)\..+values (?P<currency>[A-Z]+) (?P<value_unit>[A-Za-z]+)\.",
-	re.IGNORECASE
-)
-
-VALUE_UNITS = {
-	"thousands": 1000,
-	"millions": 1000000,
-	"billions": 1000000000,
-}
-
-# these columns have value_unit 1
-# percentage columns need not be listed here, because they are auto-recognized to have value_unit 1
-UNO_VALUE_UNIT_COLUMNS = {
-	"EPS (Basic)",
-	"EPS (Diluted)",
-	"Asset Turnover",
-	"Current Ratio",
-	"Quick Ratio",
-	"Cash Ratio"
 }
 
 def load_historical_data(archive_directory, symbol, columns=None):
@@ -64,78 +39,6 @@ def load_historical_data_multiple(archive_directory, symbols, columns=None):
 	"""
 	return pd.Panel({s: load_historical_data(archive_directory, s, columns) for s in symbols})
 
-def _parse_top_remark(string):
-	""" top remark string -> month (integer) where fiscal year ends, currency (string), value unit (multiple of 1,000) """
-	result = TOP_REMARK_RE.match(string)
-
-	if result:
-		end_month = result.group("fiscal_year_end_month")
-		currency = result.group("currency")
-		value_unit = result.group("value_unit")
-
-		return datetime.strptime(end_month, "%B").month, currency.upper(), VALUE_UNITS[value_unit.lower()]
-	else:
-		raise ValueError("Top remark cannot be parsed.")
-
-def _calc_financial_data_date(fiscal_year_end_month, period):
-	""" Returns a numpy datetime64 object which is the date after the fiscal year of <period> just ended.
-		fiscal_year_end_month: 1~12
-		period: e.g. 2013
-	"""
-	d = date(period, fiscal_year_end_month, 1) + relativedelta(months=1)
-	return np.datetime64(d)
-
-def _deduplicate_column_name(data, extract_columns):
-	""" Yields (deduplicated column name, values) by iterating over <data> where column name is in <extract_columns>,
-		append _incremental number to each duplicate column name.
-		data: a sequence of (column_name, values)
-		extract_columns: set of column name strings, None - extract all.
-	"""
-	counter = defaultdict(int)
-	for column_name, values in data:
-		if extract_columns is None or column_name in extract_columns:
-			counter[column_name] += 1
-
-			suffix = '' if counter[column_name] == 1 else '_{0}'.format(counter[column_name])
-			yield column_name + suffix, values
-
-def _parse_value(string, value_unit):
-	""" value string -> numpy-typed value
-		value_unit: a number which the resultant value except percentage will be multiplifed by
-	"""
-	if string == '-':
-		return np.nan
-	else:
-		s = string.replace(',','')
-		if s.endswith('%'):
-			return np.float64(s.rstrip('%')) / 100
-		else:
-			s = s.replace('(', '-').rstrip(')')
-			if '.' in s:
-				return np.float64(s) * value_unit
-			else:
-				return np.int64(s) * value_unit
-
-def _preprocess(obj, extract_columns):
-	""" Preprocesses <obj>, extracts columns in <extract_columns>, parses the values
-		yields column name, list of parsed values
-
-		obj: object parsed from json.
-		extract_columns: set of column name strings, None - extract all.
-	"""
-	assert obj["timeframe"] == "annual"
-
-	fiscal_year_end_month, currency, value_unit = _parse_top_remark(obj["top_remark"])
-	assert currency == "SGD"
-	calc_date = partial(_calc_financial_data_date, fiscal_year_end_month)
-
-	first_row = obj["data"][0]
-	assert first_row[0] == "periods"
-	yield "Date", [calc_date(int(d)) for d in first_row[1]]
-
-	for column_name, values in _deduplicate_column_name(obj["data"][1:], extract_columns):
-		value_unit_to_use = 1 if column_name in UNO_VALUE_UNIT_COLUMNS else value_unit
-		yield column_name, [_parse_value(v, value_unit_to_use) for v in values]
 
 def load_financial_data(archive_directory, report_type, symbol, columns=None):
 	""" Returns a DataFrame object containing <report_type> financial data of <symbol> with <columns>, loaded from <archive_directory>.
@@ -149,7 +52,7 @@ def load_financial_data(archive_directory, report_type, symbol, columns=None):
 
 	extract_columns = None if columns is None else set(columns)
 
-	df = pd.DataFrame.from_items(list(_preprocess(j, extract_columns)))
+	df = pd.DataFrame.from_items(list(preprocess(j, extract_columns)))
 	df.set_index("Date", inplace=True)
 	df.sort_index(inplace=True)
 
---
+++ tests/archive/test_preprocess.py
@@ -0,0 +1,100 @@
+import unittest
+from unittest.mock import patch, MagicMock
+
+import numpy as np
+
+from fa.archive import preprocess
+
+
+class TestPreprocess(unittest.TestCase):
+    def test_parse_top_remark(self):
+        self.assertEqual(
+            preprocess._parse_top_remark("Fiscal year is April-March. All values sgd Thousands."),
+            (3, "SGD", 1000)
+        )
+        self.assertEqual(
+            preprocess._parse_top_remark("Fiscal year is April-March. All values SGD Millions."),
+            (3, "SGD", 1000000)
+        )
+        self.assertEqual(
+            preprocess._parse_top_remark("Fiscal year is April-March. All values SGD Billions."),
+            (3, "SGD", 1000000000)
+        )
+        self.assertRaises(
+            ValueError,
+            preprocess._parse_top_remark,
+            "abcdef"
+        )
+
+    def test_deduplicate_column_name(self):
+        data = (
+            ("Sales", [1]),
+            ("Cost", [2]),
+            ("Cost", [3]),
+            ("Tax", [4]),
+            ("Cost", [5]),
+            ("Tax", [6]),
+        )
+
+        self.assertEqual(list(preprocess._deduplicate_column_name(data, extract_columns=None)), [
+            ("Sales", [1]),
+            ("Cost", [2]),
+            ("Cost_2", [3]),
+            ("Tax", [4]),
+            ("Cost_3", [5]),
+            ("Tax_2", [6]),
+        ])
+        self.assertEqual(list(preprocess._deduplicate_column_name(data, {"Sales"})), [
+            ("Sales", [1]),
+        ])
+
+    def test_parse_value(self):
+        parsed = preprocess._parse_value("3,456", value_unit=10)
+
+        self.assertTrue(np.isnan(preprocess._parse_value('-', 10)))
+        self.assertEqual(preprocess._parse_value("25%", 10), 0.25)
+        self.assertEqual(parsed, 34560)
+        self.assertIsInstance(parsed, np.int64)
+        self.assertEqual(preprocess._parse_value("(3,456)", 10), -34560)
+        self.assertEqual(preprocess._parse_value("3,456.78", 10), 34567.8)
+        self.assertEqual(preprocess._parse_value("(3,456.78)", 10), -34567.8)
+
+    def test_preprocess(self):
+        value_data = [
+            ["Cash & Short Term Investments", ["4,504", "5,400", "5,488"]],
+            ["Liabilities & Shareholders' Equity", ["25,169", "22,589", "22,501"]],
+        ]
+        obj = {
+          "data": [["periods", ["2009", "2010", "2011"]]] + value_data,
+          "symbol": "C6L",
+          "timeframe": "annual",
+          "top_remark": "top_remark",
+          "report_type": "balance-sheet",
+        }
+        extract_columns = {"column1", "column2"}
+
+        with patch("fa.archive.preprocess.UNO_VALUE_UNIT_COLUMNS", {"Cash & Short Term Investments"}), \
+             patch("fa.archive.preprocess._parse_top_remark", MagicMock(return_value=(3, "SGD", 1000000))) \
+                as mock_parse_top_remark, \
+             patch("fa.archive.preprocess._deduplicate_column_name", MagicMock(side_effect=lambda a, b: a)) \
+                as mock_deduplicate_column_name, \
+             patch("fa.archive.preprocess._parse_value", MagicMock(side_effect=lambda a, b: a)) \
+                as mock_parse_value:
+
+            preprocessed = list(preprocess.preprocess(obj, extract_columns))
+
+            mock_parse_top_remark.assert_called_once_with("top_remark")
+            mock_deduplicate_column_name.assert_called_once_with(value_data, extract_columns)
+            # "Cash & Short Term Investments" is in UNO_VALUE_UNIT_COLUMNS
+            mock_parse_value.assert_any_call("4,504", 1)
+            mock_parse_value.assert_any_call("22,501", 1000000)
+
+        self.assertEqual(preprocessed, [
+            # 04 because of mock_parse_top_remark's return value
+            ("Date", [np.datetime64(s) for s in ("2009-04-01", "2010-04-01", "2011-04-01")]),
+            ("Cash & Short Term Investments", ["4,504", "5,400", "5,488"]),
+            ("Liabilities & Shareholders' Equity", ["25,169", "22,589", "22,501"])
+        ])
+
+if __name__ == "__main__":
+    unittest.main()
