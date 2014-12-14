35820636bd67e57ffc82053da689f4e1a2280daf
Wong Jiang Fung <kakarukeys@gmail.com>
2014-12-14 18:27:12 +0800

    added database models for fundamental analysis

---
+++ fa/database/models.py
@@ -0,0 +1,78 @@
+import re
+import peewee as pw
+
+from fa.database import balancesheet_numerical_columns, cashflow_numerical_columns, incomestatement_numerical_columns
+
+
+db = pw.SqliteDatabase(None) # path to be specified at runtime
+
+#------------------------------
+# base models to subclass from
+#------------------------------
+
+class BaseModel(pw.Model):
+    class Meta:
+        database = db
+
+class EventModel(pw.Model):
+    symbol = pw.CharField(index=True)
+    date = pw.DateField()
+
+    class Meta:
+        database = db
+        indexes = (
+            # create a unique composite index
+            (("symbol", "date"), True),
+        )
+
+#------------------------------
+# models for fundamental analytis
+#------------------------------
+
+class Stock(BaseModel):
+    symbol = pw.CharField(primary_key=True)
+    name = pw.CharField(null=True)
+    industry = pw.CharField(null=True)
+    sector = pw.CharField(null=True)
+
+class MarketIndex(BaseModel):
+    symbol = pw.CharField(primary_key=True)
+    name = pw.CharField(null=True)
+
+class Price(EventModel):
+    open = pw.DoubleField()
+    high = pw.DoubleField()
+    low = pw.DoubleField()
+    close = pw.DoubleField()
+    volume = pw.BigIntegerField()
+    adj_close = pw.DoubleField()
+
+# the 3 financial data models are dynamically generated:
+
+def _to_pythonic_name(verbose_name):
+    return re.sub(r"[^\w]+", '_', verbose_name).lower()
+
+def _get_numerical_column_names(model_name):
+    """returns all numerical column names of a financial data model,
+       which are converted from verbose names stored in separate files imported.
+    """
+    verbose_names = globals()[model_name.lower() + "_numerical_columns"].verbose_names
+    return [_to_pythonic_name(name) for name in verbose_names]
+
+def _create_financial_data_model(model_name):
+    column_names = _get_numerical_column_names(model_name)
+
+    return type(
+        model_name,
+        (EventModel,),
+        {name: pw.DoubleField(null=True) for name in column_names}
+    )
+
+for model_name in ("BalanceSheet", "CashFlow", "IncomeStatement"):
+    globals()[model_name] = _create_financial_data_model(model_name)
+
+#------------------------------------------------
+# lists all active models (for iteration purpose)
+#------------------------------------------------
+
+export = [Stock, MarketIndex, Price, BalanceSheet, CashFlow, IncomeStatement]
---
+++ tests/database/test_models.py
@@ -0,0 +1,58 @@
+import unittest
+from unittest.mock import patch, MagicMock, call
+from datetime import date
+
+import peewee as pw
+
+from fa.database import models
+from tests.util import DBTestCase
+
+
+class TestModels(DBTestCase):
+    def test_whether_models_use_database(self):
+        for model in models.export:
+            self.assertIs(model._meta.database, models.db)
+
+    def test_models(self):
+        symbol = "C6L.SI"
+
+        try:
+            models.Stock.create(symbol=symbol)
+            models.MarketIndex.create(symbol=symbol)
+            models.Price.create(symbol=symbol, date=date.today(), open=1.0, close=1.0, high=1.0, low=1.0, volume=10, adj_close=1.0)
+            models.BalanceSheet.create(symbol=symbol, date=date.today(), cash_only=123.45)
+            models.CashFlow.create(symbol=symbol, date=date.today(), free_cash_flow=456.78)
+            models.IncomeStatement.create(symbol=symbol, date=date.today(), ebit_margin=901.23)
+
+            for model in models.export:
+                self.assertEqual(model.get().symbol, "C6L.SI")
+
+            self.assertEqual(models.BalanceSheet.get().cash_only, 123.45)
+            self.assertEqual(models.CashFlow.get().free_cash_flow, 456.78)
+            self.assertEqual(models.IncomeStatement.get().ebit_margin, 901.23)
+
+        except (pw.DatabaseError, NameError):
+            self.fail("models not declared properly!")
+
+    def test_to_pythonic_name(self):
+        name = models._to_pythonic_name("Cash & ST Investments / Total Assets")
+        self.assertEqual(name, "cash_st_investments_total_assets")
+
+    def test_get_numerical_column_names(self):
+        with patch("fa.database.balancesheet_numerical_columns.verbose_names", ["abc", "def"]), \
+             patch("fa.database.models._to_pythonic_name", MagicMock(side_effect=lambda x: x + '_')) as mock_to_pythonic_name:
+            names = models._get_numerical_column_names("BalanceSheet")
+            mock_to_pythonic_name.assert_has_calls([call("abc"), call("def")])
+
+        self.assertEqual(names, ["abc_", "def_"])
+
+    def test_numerical_column_names_uniqueness(self):
+        for model_name in ("BalanceSheet", "CashFlow", "IncomeStatement"):
+            names = models._get_numerical_column_names(model_name)
+            self.assertTrue(
+                len(names) == len(set(names)),
+                "two verbose column names in {0} are mapped to the same pythonic name!".format(model_name)
+            )
+
+if __name__ == "__main__":
+    unittest.main()
