0e2a50d45b5ccd2f30c000219a6e60ae6dcfb10c
Wong Jiang Fung <kakarukeys@gmail.com>
2014-12-21 14:25:50 +0800

    added routine to update historical prices

--- fa/database/query.py
+++ fa/database/query.py
@@ -1,5 +1,10 @@
-from fa.database.models import Symbol, export
+import csv
+import logging
 
+from fa.database.models import db, Symbol, Price, export
+
+
+logger = logging.getLogger(__name__)
 
 def get_symbols_to_update_data(end_date):
     """ Gets symbols which data is never updated or was updated before <end_date>
@@ -8,6 +13,23 @@ def get_symbols_to_update_data(end_date):
     return [s.symbol for s in Symbol.select(Symbol.symbol).where(
         (Symbol.price_updated_at < end_date) | (Symbol.price_updated_at == None)
     )]
+
+def update_historical_prices(symbol, records, end_date):
+    """ Updates historical prices of <symbol> with <records> and mark it as updated at <end_date>.
+        records: a sequence of maps
+        end_date: datetime object
+    """
+    logger.info("Updating historical prices of {0} in database...".format(symbol))
+    try:
+        with db.transaction():
+            Price.delete().where(Price.symbol_obj == symbol).execute()  # there may be adjustment to existing prices
+            for rec in records:
+                Price.create(**rec)
+            Symbol.update(price_updated_at=end_date).where(Symbol.symbol == symbol).execute()
+    except Exception as e:
+        logger.exception(e)
+        logger.error("Historical prices of {0} are not updated.".format(symbol))
+        raise
 
 def delete_all():
     for Model in export:
--- tests/database/test_query.py
+++ tests/database/test_query.py
@@ -1,18 +1,28 @@
 import unittest
 from datetime import datetime
 
-from fa.database import models, query
+import peewee as pw
+
+from fa.database import query
+from fa.database.models import *
 from tests.util import DBTestCase
 
 
 class TestQuery(DBTestCase):
     def setUp(self):
-        with models.db.transaction():
-            models.Symbol.insert_many([
-                {"symbol": "C6L.SI", "price_updated_at": None},
-                {"symbol": "J7X.SI", "price_updated_at": datetime(2012, 12, 21)},
-                {"symbol": "ABC.SI", "price_updated_at": datetime(2013, 1, 1)},
-            ]).execute()
+        self.symbols = [
+            {"symbol": "C6L.SI", "price_updated_at": None},
+            {"symbol": "J7X.SI", "price_updated_at": datetime(2012, 12, 21)},
+            {"symbol": "ABC.SI", "price_updated_at": datetime(2013, 1, 1)},
+        ]
+
+        self.prices = [
+            {"symbol_obj": "C6L.SI", "date": datetime(2012, 12, 21), "open": 0, "close": 0, "high": 0, "low": 0, "volume": 0, "adj_close": 99.9},
+        ]
+
+        with db.transaction():
+            Symbol.insert_many(self.symbols).execute()
+            Price.insert_many(self.prices).execute()
 
     def tearDown(self):
         query.delete_all()
@@ -21,9 +31,48 @@ class TestQuery(DBTestCase):
         symbols = query.get_symbols_to_update_data(datetime(2013, 1, 1))
         self.assertEqual(symbols, ["C6L.SI", "J7X.SI"])
 
+    def test_update_historical_prices(self):
+        symbol = "C6L.SI"
+        records = [
+            {"symbol_obj": "C6L.SI", "date": datetime(2012, 12, 21), "open": 0, "close": 0, "high": 0, "low": 0, "volume": 0, "adj_close": 100.0},
+            {"symbol_obj": "C6L.SI", "date": datetime(2012, 12, 22), "open": 0, "close": 0, "high": 0, "low": 0, "volume": 0, "adj_close": 120.0},
+        ]
+        end_date = datetime(2012, 12, 22)
+
+        query.update_historical_prices(symbol, records, end_date)
+
+        updated_prices = list(Price.select().where(Price.symbol_obj == symbol).dicts())
+        for p in updated_prices:
+            del p["id"]
+
+        marker = Symbol.select(Symbol.price_updated_at).where(Symbol.symbol == symbol).get().price_updated_at
+
+        self.assertEqual(updated_prices, records)
+        self.assertEqual(marker, end_date)
+
+    def test_update_historical_prices_exception_handling(self):
+        symbol = "C6L.SI"
+        records = [ # duplicate events will trigger integrity error
+            {"symbol_obj": "C6L.SI", "date": datetime(2012, 12, 22), "open": 0, "close": 0, "high": 0, "low": 0, "volume": 0, "adj_close": 100.0},
+            {"symbol_obj": "C6L.SI", "date": datetime(2012, 12, 22), "open": 0, "close": 0, "high": 0, "low": 0, "volume": 0, "adj_close": 120.0},
+        ]
+        end_date = datetime(2012, 12, 22)
+
+        self.assertRaises(pw.IntegrityError, query.update_historical_prices, symbol, records, end_date)
+
+        # rollback should occur
+        updated_prices = list(Price.select().where(Price.symbol_obj == symbol).dicts())
+        for p in updated_prices:
+            del p["id"]
+
+        marker = Symbol.select(Symbol.price_updated_at).where(Symbol.symbol == symbol).get().price_updated_at
+
+        self.assertEqual(updated_prices, self.prices)
+        self.assertIs(marker, None)
+
     def test_delete_all(self):
         query.delete_all()
-        self.assertEqual(list(models.Symbol.select()), [])
+        self.assertEqual(list(Symbol.select()), [])
 
 if __name__ == "__main__":
     unittest.main()
