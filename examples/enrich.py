468578f7a26e2325ea57d304b547368db55f5ca0
Wong Jiang Fung <kakarukeys@gmail.com>
2015-03-23 22:23:03 +0800

    removed useless files rewritten enrich.py with database operations

--- examples/enrich.py
+++ examples/enrich.py
@@ -1,14 +1,16 @@
-from fa.archive.load import load_financial_data
-from fa.archive.save import dump
-
-from settings import symbols, archive_directory, wsj_archive_directory
+from fa.database.models import IncomeStatement
+import initialize
 
 
 """ Enrich financial data with extra data columns calculated from existing columns """
 
-if __name__ == "__main__":
-    for s in symbols:
-        df = load_financial_data(archive_directory, "income-statement", s)
-        insert_index = df.columns.get_loc("Other Operating Expense") + 1
-        df.insert(insert_index, "Operating Income", df["Gross Income"] - df["SG&A Expense"] - df["Other Operating Expense"])
-        dump({s: df}, wsj_archive_directory, "income-statement")
+initialize.init()
+
+IncomeStatement \
+    .update(operating_income=(
+        IncomeStatement.gross_income -
+        IncomeStatement.sg_a_expense -
+        IncomeStatement.other_operating_expense
+    )) \
+    .where(IncomeStatement.operating_income >> None) \
+    .execute()
